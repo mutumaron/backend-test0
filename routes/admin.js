9929e5c43b42430b4359cb064dadb918a73445a9
mutumaron <mutembeironny11@gmail.com>
2024-04-29 21:58:36 +0300

    rm auth

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,22 @@
 const express = require("express");
 
 const adminController = require("../controllers/adminController");
-const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
-router.get("/add-content", isAuth, adminController.getAddContent);
-router.get("/add-blog", isAuth, adminController.getAddBlog);
+router.get("/add-content",adminController.getAddContent);
+router.get("/add-blog",adminController.getAddBlog);
 
-router.get("/edit-deal/:dealId", isAuth, adminController.getEditDeal);
-router.post("/edit-deal",  isAuth,adminController.postEditDeal);
-router.get("/edit-blog/:blogId", isAuth, adminController.getEditBlog);
-router.post("/edit-blog", isAuth, adminController.postEditBlog);
+router.get("/edit-deal/:dealId", adminController.getEditDeal);
+router.post("/edit-deal", adminController.postEditDeal);
+router.get("/edit-blog/:blogId",adminController.getEditBlog);
+router.post("/edit-blog", adminController.postEditBlog);
 
 
-router.post("/deal/add-deal",  isAuth,adminController.postAddDeal);
-router.post("/blog/add-blog",  isAuth,adminController.postAddBlog);
+router.post("/deal/add-deal", adminController.postAddDeal);
+router.post("/blog/add-blog", adminController.postAddBlog);
 
-router.post("/delete-deal",  isAuth,adminController.postDeleteDeal);
-router.post("/delete-blog",  isAuth,adminController.postDeleteBlog);
+router.post("/delete-deal",  adminController.postDeleteDeal);
+router.post("/delete-blog",  adminController.postDeleteBlog);
 
 module.exports = router;
